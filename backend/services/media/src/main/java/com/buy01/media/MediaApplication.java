000593114510780ad98a3b7fd2b7c9d395247adf
wiwa-dev <63481064+wiwa-dev@users.noreply.github.com>
2026-02-06 13:11:05 +0000

    Update comment in MediaApplication.java

--- backend/services/media/src/main/java/com/buy01/media/MediaApplication.java
+++ backend/services/media/src/main/java/com/buy01/media/MediaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-// test1
+// test
 public class MediaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MediaApplication.class, args);
